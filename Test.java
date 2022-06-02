d85712e26333134e98c853ccec3131d8e0c44f32
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-02 22:14:04 +0800

    Update Test.java

--- Test.java
+++ Test.java
@@ -2,14 +2,16 @@ public class Test {
     public static void main(String[] args) {
         int x = 34;
         int y = 43;
-        int temp = null;
+        int temp;
+        int T;
 
-        int temp = x;
-        int x = y;
-        int y = temp;
+        temp = x;
+        x = y;
+        y = temp;
+        T = x + y;
 
-        System.out.println(x);
-        System.out.println(y);
-        System.out.println(x + y);
+        System.out.println("x: " + x);
+        System.out.println("y: " + y);
+        System.out.println("Total: " + T);
     }
 }
