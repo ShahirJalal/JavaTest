2a8aaa224ef47bfb5044c32954453c387507d801
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-04 22:00:35 +0800

    Update Test1.java

--- Test1.java
+++ Test1.java
@@ -1,9 +1,9 @@
-public class Test1 {
-    public static void main(String[] args) {
-        String Mamak[];
+// public class Test1 {
+//     public static void main(String[] args) {
+//         String Mamak[];
 
-        list Mamak[] {"Wan" , "Dharmesh" , "Bul" , "Shahir"};
-        System.out.println(Mamak);
+//         list Mamak[] {"Wan" , "Dharmesh" , "Bul" , "Shahir"};
+//         System.out.println(Mamak);
 
-    }
-}
+//     }
+// }
