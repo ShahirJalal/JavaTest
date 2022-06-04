f881c7ba272787fa565f79a03a53794ea08a57d7
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-04 21:57:37 +0800

    Update Test2.java

--- Test2.java
+++ Test2.java
@@ -2,8 +2,11 @@ import java.util.Scanner;
 
 public class Test2 {
     public static void main(String[] args) {
-        Scanner NameInput = new Scanner(System.in);
+        Scanner nameInput = new Scanner(System.in);
 
-        System.out.println("What is your name?");
+        System.out.println("What is your name? ");
+        String name = nameInput.nextLine();
+
+        System.out.println("Your name is: " + name);
     }
 }
