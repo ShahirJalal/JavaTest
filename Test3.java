abb99b627129335b3fd653516f0c3eea6b19445f
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-05 23:56:31 +0800

    Update Test3.java

--- Test3.java
+++ Test3.java
@@ -1,8 +1,11 @@
 public class Test3 {
     public static void main(String[] args) {
         int friends = 10;
+        int foes = 0;
 
         friends = friends + 1;
+        foes++;
         System.out.println(friends);
+        System.out.println(foes);
     }
 }
