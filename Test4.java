f07578036fa6facd0912044bdbeb3717630be35d
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-06 16:14:43 +0800

    Update Test4.java

--- Test4.java
+++ Test4.java
@@ -2,6 +2,6 @@ import javax.swing.JOptionPane;
 
 public class Test4 {
     public static void main(String[] args) {
-        
+        String name = JOptionPane.showInputDialog("Enter your name: ");
     }
 }
