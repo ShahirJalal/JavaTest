766758efabcc4477a95b3c155cce5668e0192c59
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-08 01:05:22 +0800

    Create Test20.java

---
+++ Test20.java
@@ -0,0 +1,24 @@
+public class Test20 {
+    public static void main(String[] args) {
+        
+        // wrapper class = provides a way to use primitive data types as reference data types
+        //                 reference data types contain useful methods
+        //                 can be used with collections (ex. ArrayList)
+
+        //primitive     //wrapper
+        //---------     //-------
+        // boolean      Boolean
+        // char         Char
+        // int          Integer
+        // double       Double
+
+        // autoboxing = the automatic conversion that the Java compiler makes between the primitive data types
+        // unboxing   = the reverse of autoboxing. Automatic conversion of wrapper class to primite
+
+        Boolean a = true;
+        Character b = '@';
+        Integer c = 3;
+        Double d = 4.5;
+        String e = "Awesome";
+    }
+}
