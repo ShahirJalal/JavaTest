773e7615a9c0df3f9426d15a31d9500d8b4ea595
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-07 23:21:31 +0800

    Update Test5.java

--- Test5.java
+++ Test5.java
@@ -1,3 +1,11 @@
 public class Test5 {
-    
+    public static void main(String[] args) {
+        double x = 3.14;
+        double y = -10;
+
+        double z = Math.max(x, y);
+
+        System.out.println(z);
+        
+    }
 }
