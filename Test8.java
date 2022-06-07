6392d02d7ae5a74f3a673b2e0f269cf2737fd627
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-07 23:37:38 +0800

    Create Test8.java

---
+++ Test8.java
@@ -0,0 +1,16 @@
+public class Test8 {
+    public static void main(String[] args) {
+        
+        int age = 12;
+
+        if(age >= 18) {
+            System.out.println("You are an adult!");
+        }
+        else if(age >= 75) {
+            System.out.println("OK Boomer!");
+        }
+        else {
+            System.out.println("You are not an adult!");
+        }
+    }
+}
