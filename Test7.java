96aeefecd19f59b0f978c19eb6e74c0e94107c7f
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-07 23:31:21 +0800

    Create Test7.java

---
+++ Test7.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class Test7 {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        // int x = random.nextInt(6)+1;
+        double y = random.nextDouble();
+
+        System.out.println(y);
+    }
+}
