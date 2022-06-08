1d6c51cd6328d8bf65b35f216158c37df8bf2ded
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-08 19:39:31 +0800

    Update

---
+++ Test25.java
@@ -0,0 +1,21 @@
+public class Test25 {
+    public static void main(String[] args) {
+
+        // method
+        
+        int x = 3;
+        int y = 4;
+
+        int z = add(x, y);
+
+        System.out.println(z);
+        System.out.println(add(x, y));
+
+    }
+
+    static int add(int x, int y) {
+
+        int w = x + y;
+        return 0;
+    }
+}
