c68f131b0b6a1e45f83bd8f440b48f6c2a14e78a
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-09 23:23:38 +0800

    Update

--- Test33/Car.java
+++ Test33/Car.java
@@ -6,5 +6,11 @@ public class Car {
     String model = "Mustang";
     String color = "red";
     int year = 2011;
+
+    public String toString() {
+    
+        return make + "\n" + model + "\n" + color + "\n" + year;
+
+    }
     
 }
---
+++ Test34/Food.java
@@ -0,0 +1,10 @@
+package Test34;
+
+public class Food {
+    
+    String name;
+
+    Food(String name) {
+        this.name = name;
+    }
+}
