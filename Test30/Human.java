f8b101d4fee9c6bc056051ae10dd26d319949e53
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-09 21:59:21 +0800

    Update

---
+++ Test30/Human.java
@@ -0,0 +1,17 @@
+package Test30;
+
+public class Human {
+
+    String name;
+    int age;
+    double weight;
+
+    Human(String name, int age, double weight) {
+
+        this.name = name;
+        this.age = age;
+        this.weight = weight;
+
+    }
+
+}
---
+++ Test30/Main.java
@@ -0,0 +1,11 @@
+package Test30;
+
+public class Main {
+    public static void main(String[] args) {
+        
+        Human human = new Human("Rick" , 65 , 70);
+        Human human2 = new Human("Morty" , 16 ,50);
+
+        System.out.println(human2.name);
+    }
+}
