d8d3b2fd5a26cbaec386065b05b6e4b82a6a8930
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-13 22:12:20 +0800

    Update Hero.java

--- Test41/Hero.java
+++ Test41/Hero.java
@@ -2,8 +2,7 @@ public class Hero extends Person{
     String power;
 
     Hero(String name, int age, String power) {
-        this.name = name;
-        this.age = age;
+        super(name, age);
         this.power = power;
     }
 }
