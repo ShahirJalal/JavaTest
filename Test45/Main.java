4ea767541f97177be962ad9bdd514540742961ed
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-14 23:30:20 +0800

    Update

---
+++ Test45/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        
+        Car car = new Car("Chevrolet", "Camaro", 2021);
+
+        System.out.println(car.getMake());
+    }
+
+}
