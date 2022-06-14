c8da496371cfec02e73b58954e554394ba9eac92
Shahir Jalal <103807467+ShahirJalal@users.noreply.github.com>
2022-06-14 22:38:35 +0800

    Update

---
+++ Test43/package1/A.java
@@ -0,0 +1,11 @@
+package package1;
+import package2.*;
+
+public class A {
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+        C c = new C();
+        System.out.println(c.defaultMessage);
+    }
+}
